e9c7d21a51f66567dad48759c7aa5b18b7f84634
Ricardo Roman <ricardoromanamigo@gmail.com>
2026-06-02 19:20:45 +0200

    Application Update commit
    - Update IUserAddressRepository.cs file

--- src/PolyStore.Application/Abstractions/Persistence/IUserAddressRepository.cs
+++ src/PolyStore.Application/Abstractions/Persistence/IUserAddressRepository.cs
@@ -4,5 +4,8 @@ namespace PolyStore.Application.Abstractions.Persistence;
 
 public interface IUserAddressRepository
 {
+    Task<UserAddress?> GetByUserIdAsync(Guid userId);
     Task AddAsync(UserAddress address);
+    Task UpdateAsync(UserAddress address);
+    Task SaveChangesAsync();
 }
