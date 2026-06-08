8ec2667f0f7f26eb0cfd3db390b3485c165af966
Ricardo Roman <ricardoromanamigo@gmail.com>
2026-06-08 16:53:45 +0200

    Infrastructure update commit
    New Migration and DB Update

---
+++ src/PolyStore.Infrastructure/Migrations/20260608144724_AddPaymentDateToOrder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PolyStore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentDateToOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "PaymentDate",
+                table: "Orders",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Orders");
+        }
+    }
+}
